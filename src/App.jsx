f0f50a6037657f5f5cbc919ee636329afd64e3e9
patel-param-09 <patelparam9304@gmail.com>
2024-07-02 12:10:09 +0530

    2.8: The Phonebook Step 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,33 @@ import { useState } from "react";
 const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
+  const [newPhn, setPhn] = useState("");
   const [List, setList] = useState([]);
 
   function handleSubmit(event) {
     event.preventDefault();
     setNewName("");
-    return <li>{setList([...List, newName])}</li>;
+    setPhn("");
+    setList([...List, { newName, newPhn }]);
   }
 
   function handleChange(e) {
     setNewName(e.target.value);
   }
+
+  function handleChange2(e) {
+    setPhn(e.target.value);
+  }
+
+  let showData = <h5>No Data Available</h5>;
+
+  showData = List.map((element, i) => {
+    return (
+      <li key={i}>
+        {element.newName} {element.newPhn}
+      </li>
+    );
+  });
 
   return (
     <div>
@@ -23,11 +39,15 @@ const App = () => {
           name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
+          number: <input value={newPhn} onChange={handleChange2} />
+        </div>
+        <br />
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {List}
+      <ul>{showData}</ul>
     </div>
   );
 };
