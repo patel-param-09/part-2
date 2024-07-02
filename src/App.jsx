82ee931460ea17c41230ed710b39260a75e6c8af
patel-param-09 <patelparam9304@gmail.com>
2024-07-02 11:55:17 +0530

    2.6: The Phonebook Step 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,34 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const App = () => {
-  const courses = [
-    {
-      name: "Half Stack application development",
-      id: 1,
-      parts: [
-        {
-          name: "Fundamentals of React",
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: "Using props to pass data",
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: "State of a component",
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: "Redux",
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: "Node.js",
-      id: 2,
-      parts: [
-        {
-          name: "Routing",
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: "Middlewares",
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ];
-  // id = 1
-  const total = courses[0].parts.reduce(
-    (prev, curr) => prev + curr.exercises,
-    0
-  );
-  const Element1 = courses[0].parts.map((Element) => {
-    return (
-      <li key={Element.id}>
-        {Element.name} {Element.exercises}
-      </li>
-    );
-  });
-  // id = 2
-  const total2 = courses[1].parts.reduce(
-    (prev, curr) => prev + curr.exercises,
-    0
-  );
-  const Element2 = courses[1].parts.map((Element) => {
-    return (
-      <li key={Element.id}>
-        {Element.name} {Element.exercises}
-      </li>
-    );
-  });
-  // Return Statement
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  const [List, setList] = useState([]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setNewName("");
+    return <li>{setList([...List, newName])}</li>;
+  }
+
+  function handleChange(e) {
+    setNewName(e.target.value);
+  }
+
   return (
-    <>
-      <h1>{courses[0].name}</h1>
-      {Element1}
-      <h3>Total of {total} Exercises.</h3>
-      <hr />
-      <h1>{courses[1].name}</h1>
-      {Element2}
-      <h3>Total of {total2} Exercises.</h3>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {List}
+    </div>
   );
 };
 
